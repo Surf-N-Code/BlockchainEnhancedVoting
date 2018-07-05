54c2da6b2b7a120c978ac7e31a7eee4fe4b21543
Norman <ndilthey@gmail.com>
2018-07-05 09:46:13 +0200

    Smart contract with duration, timeout, onlyOwner function, set contract duration externally, voters can only vote once, contract stages set automatically

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,40 +1,162 @@
 pragma solidity ^0.4.2;
 
 contract Election {
+
+    //Candidates
+    struct Candidate {
+        uint id;
+        uint8 voteCount;
+        string name;
+    }
+    //Question Categories
+    struct Categories {
+        uint id;
+        string name;
+        uint[] candidateList;
+        mapping(uint => Candidate) candidateStructs;
+    }
+
     //Contract stages
     enum Stages {
         AcceptingBlindVotes,
-        RevealResults,
-        GettingDiscordMembers,
-        StartingNewVote
+        RevealResults
     }
-    //Define categories array
-    uint8[5][] categories;
-    //category[1][UID] = 10 votes;
 
-    //Candidatescount per category
-    uint8[5] candidatesCountPerCategory;
-
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    //Store Categories by Id
+    mapping(uint => Categories) public categoryStructs;
+    //Read/write candidates
+    mapping(uint => Candidate) public candidates;
+    //Store accounts that have voted
+    mapping(address => bool) public voters;
+    //Election start and Enddates initialized in constructor
     uint public electionEnd;
     uint public electionStart;
+    uint public electionDuration;
+    uint public revealResultDuration;
     //Start with the voting stage
     Stages public stage = Stages.AcceptingBlindVotes;
+    //Owner address
+    address public owner = msg.sender;
 
-    //Create a block whether a function can be called within the current stage
-    modifier atStage(Stages _stage) {
+    modifier onlyByMe(address _account)
+    {
         require(
-            stage == _stage,
-            "Function cannot be called at this time."
+            msg.sender == _account,
+            "Sender not authorized."
         );
         _;
+    }
+    function changeOwner(address _newOwner)
+            public
+            onlyBy(owner)
+    {
+        owner = _newOwner;
+    }
+
+    function getCategory(uint8 _categoryId)
+        public
+        view
+        returns(string name, uint candidateCount)
+    {
+        return(categoryStructs[_categoryId].name, categoryStructs[_categoryId].candidateList.length);
+    }
+
+    function getCandidateResultsAndInfoFromCategory(uint8 _categoryId, uint _candidateId)
+        public
+        view
+        returns(uint id, string name, uint8 voteCount)
+    {
+        return(
+            categoryStructs[_categoryId].candidateStructs[_candidateId].id,
+            categoryStructs[_categoryId].candidateStructs[_candidateId].name,
+            categoryStructs[_categoryId].candidateStructs[_candidateId].voteCount
+            );
+    }
+
+    function addVoteForCandidateInCategory(uint8 _categoryId, uint _candidateId)
+        public
+        timedTransition
+        onlyWhileAcceptingVotes
+        returns(bool success)
+    {
+        //Make sure voters only vote once
+        require(!voters[msg.sender]);
+        //Make sure candidate is valid
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        if(categoryStructs[_categoryId].candidateStructs[_candidateId].voteCount == 0) {
+            categoryStructs[_categoryId].candidateList.push(_candidateId);
+        }
+        categoryStructs[_categoryId].candidateStructs[_candidateId].voteCount++;
+        // record that voter has voted
+        voters[msg.sender] = true;
+        // trigger voted event
+        votedEvent(_candidateId); //@TODO: to test this
+        return true;
+    }
+
+    //Store Candidates Count
+    uint public candidatesCount;
+
+    function Election () public {
+        /* for(uint8 i=0; i) */
+        addCandidate(3, "Candidate 3", 0);
+        addCandidate(3, "Candidate 3", 1);
+        addCandidate(3, "Candidate 3", 2);
+        addCandidate(3, "Candidate 3", 3);
+        addCandidate(3, "Candidate 3", 4);
+        addCandidate(3, "Candidate 3", 5);
+        addCandidate(3, "Candidate 3", 6);
+        addCandidate(3, "Candidate 3", 7);
+
+        addCandidate(1, "Candidate 1", 0);
+        addCandidate(1, "Candidate 1", 1);
+        addCandidate(1, "Candidate 1", 2);
+        addCandidate(1, "Candidate 1", 3);
+        addCandidate(1, "Candidate 1", 4);
+        addCandidate(1, "Candidate 1", 5);
+        addCandidate(1, "Candidate 1", 6);
+        addCandidate(1, "Candidate 1", 7);
+
+        addCandidate(2, "Candidate 2", 0);
+        addCandidate(2, "Candidate 2", 1);
+        addCandidate(2, "Candidate 2", 2);
+        addCandidate(2, "Candidate 2", 3);
+        addCandidate(2, "Candidate 2", 4);
+        addCandidate(2, "Candidate 2", 5);
+        addCandidate(2, "Candidate 2", 6);
+        addCandidate(2, "Candidate 2", 7);
+
+        //Initialize voting categories
+        newCategory(0, "Overall Best Teacher");
+        newCategory(1, "Best Altcoin Picks");
+        newCategory(2, "Best Guard on Bitcoin Watch");
+        newCategory(3, "Best ICO Contributions");
+        newCategory(4, "Best FA Analysis");
+        newCategory(5, "Best Shot Caller on Margin Plays");
+        newCategory(6, "Best Contributions to Tools and Education");
+        newCategory(7, "General Positive Influence on the Pound");
+
+        //Set the voting period
+        electionStart = now;
+        electionEnd = electionStart + electionDuration days;
+    }
+
+    function setTimeForElectionInDays()
+        external (uint8 _time)
+        onlyByMe
+    {
+        electionDuration = _time;
     }
 
     /**
      * Promotes contract into the next stage.
-     * Start with phase 0 again once StartingNewVote is complete
+     * Start with phase 0 again once RevealResults is complete
      */
     function nextStage() internal {
-        if(uint(stage) == 3) {
+        //Loop through stages and start again when RevealResults is done
+        if(uint(stage) == 1) {
             stage = Stages(0);
         } else {
             stage = Stages(uint(stage) + 1);
@@ -45,11 +167,17 @@ contract Election {
      * Creates a timed transition into the next contract stage
      */
     modifier timedTransition() {
+        //Close contract once election period ends
         if(stage == Stages.AcceptingBlindVotes
             && now >= (electionEnd)) {
                 nextStage();
         }
-        //All other states transition by transactions
+
+        //Reset into AcceptingBlindVotes once RevealResults duration has passed
+        if(stage == Stages.RevealResults && electionEnd + revealResultDuration > now) {
+
+            nextStage();
+        }
         _;
     }
 
@@ -63,74 +191,6 @@ contract Election {
         _;
     }
 
-    // Model a Candidate
-    struct Candidate {
-        uint id;
-        string name;
-        uint voteCount;
-    }
-    //Store accounts that have voted
-    mapping(address => bool) public voters;
-    // Read/write candidates
-    mapping(uint => Candidate) public candidates;
-
-    // Store Candidates Count
-    uint public candidatesCount;
-
-    function Election () public {
-        addCandidate("Candidate 1");
-        addCandidate("Candidate 2");
-        electionStart = now;
-        electionEnd = electionStart + 20 seconds;
-    }
-
-    function addCandidate (string _name) private {
-        candidatesCount ++;
-        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
-    }
-
-    function vote (uint[] _candidateIds)
-        public
-        timedTransition
-        onlyWhileAcceptingVotes {
-        // require voter hasn't voted before
-        require(!voters[msg.sender]);
-
-        // require valid candidates
-        for (i=0;i<_candidateIds.length) {
-            require(_candidateIds[i] > 0 && _candidateIds[i] <= candidatesCount);
-        }
-
-        // record that voter has voted
-        voters[msg.sender] = true;
-
-        // update candidate vote Count
-        candidates[_candidateId].voteCount ++;
-
-        // trigger voted event
-        votedEvent(_candidateId);
-    }
-
-    /* function vote (uint _candidateId)
-        public
-        timedTransition
-        onlyWhileAcceptingVotes {
-        // require that they haven't voted before
-        require(!voters[msg.sender]);
-
-        // require a valid candidate
-        require(_candidateId > 0 && _candidateId <= candidatesCount);
-
-        // record that voter has voted
-        voters[msg.sender] = true;
-
-        // update candidate vote Count
-        candidates[_candidateId].voteCount ++;
-
-        // trigger voted event
-        votedEvent(_candidateId);
-    } */
-
     event votedEvent (
         uint indexed _candidateId
     );
@@ -143,12 +203,31 @@ contract Election {
         return now >= (electionEnd);
     }
 
-    function returnNow() public view returns (uint) {
-        return now;
+    function newCategory(uint8 _categoryId, string _name)
+        onlyByMe
+        returns(bool success)
+    {
+        // not checking for duplicates
+        categoryStructs[_categoryId].id = _categoryId;
+        categoryStructs[_categoryId].name = _name;
+        //categoriesList.push(_categoryId);
+        return true;
     }
 
-    function returnEndTime() public view returns (uint) {
-        return (electionEnd);
+    function addCandidate (uint8 _candidateId, string _name, uint8 _categoryId)
+        public
+        onlyByMe
+    {
+        bool isCandidate = false;
+        for(uint i; i<8; i++) {
+            if(categoryStructs[i].candidateStructs[_candidateId]) {
+                isCandidate = true;
+            }
+        }
+        if(isCanddiate) {
+            candidatesCount++;
+        }
+        categoryStructs[_categoryId].candidateStructs[_candidateId] = Candidate(_candidateId, 0, _name);
     }
 
     //@TODO: only owner einfügen
--- src/js/app.js
+++ src/js/app.js
@@ -22,8 +22,9 @@ App = {
 
   castVote: function() {
       var candidateId = $('#candidatesSelect').val();
+      var categoryId = $('.category').attr('categoryId');
       App.contracts.Election.deployed().then(function(instance) {
-        return instance.vote(candidateId, { from: App.account });
+        return instance.addVoteForCandidateInCategory(categoryId, candidateId);
       }).then(function(result) {
         // Wait for votes to update
         $("#content").hide();
@@ -39,41 +40,6 @@ App = {
           console.log(hasClosed);
       });
   },
-  // toggleVotingResults: function() {
-  //     var resultTable = $('#voteResultsTable');
-  //     App.contracts.Election.deployed().then(function(instance) {
-  //       return instance.hasClosed();
-  //     }).then(function(hasClosed) {
-  //         if(hasClosed) {
-  //             console.log("has closed");
-  //             resultTable.show();
-  //
-  //             App.contracts.Election.deployed().then(function(instance) {
-  //               electionInstance = instance;
-  //               return electionInstance.candidatesCount();
-  //             }).then(function(candidatesCount) {
-  //               var candidatesResults = $("#candidatesResults");
-  //               candidatesResults.empty();
-  //
-  //               for (var i = 1; i <= candidatesCount; i++) {
-  //                 electionInstance.candidates(i).then(function(candidate) {
-  //                   var id = candidate[0];
-  //                   var name = candidate[1];
-  //                   var voteCount = candidate[2];
-  //
-  //                   // Render candidate Result
-  //                   var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
-  //                   candidatesResults.append(candidateTemplate);
-  //                 });
-  //               }
-  //           });
-  //             console.log("should clear interval");
-  //         } else {
-  //             console.log("open");
-  //             resultTable.hide();
-  //         }
-  //     });
-  // },
 
   listenForEvents: function() {
   App.contracts.Election.deployed().then(function(instance) {
@@ -94,12 +60,8 @@ App = {
       App.contracts.Election = TruffleContract(election);
       // Connect provider to interact with contract
       App.contracts.Election.setProvider(App.web3Provider);
-
       App.listenForEvents();
       App.render();
-      var refreshIntervalId = setInterval(function(){
-          App.toggleVotingResults(refreshIntervalId);
-      }, 5000);
       return;
     });
   },
@@ -108,6 +70,7 @@ App = {
     var electionInstance;
     var loader = $("#loader");
     var content = $("#content");
+    var resultBlock = $('#resultsBlock');
 
     loader.show();
     content.hide();
@@ -119,33 +82,26 @@ App = {
         $("#accountAddress").html("Your Account: " + account);
       }
     });
-    loader.hide();
-    content.show();
+
 
     App.contracts.Election.deployed().then(function(instance) {
+
       let candidates;
-      electionInstance = instance;
+      let candidateList;
+      let candidatesCnt;
+      let electionInstance = instance;
+
       return electionInstance.candidatesCount();
-    }).then(function(candidatesCnt) {
-        candidatesCount = candidatesCnt;
+  }).then(function(candidatesCount) {
+
+      candidatesCnt = candidatesCount;
       return electionInstance.hasClosed();
-    }).then(function(hasClosed) {
+  }).then(function(hasClosed) {
+      //@TODO change contract closing time and the !hasClosed here
         if(hasClosed) {
-            let resultTable = $('#voteResultsTable');
             console.log("Contract closed");
-            resultTable.show();
-            var candidatesResults = $("#candidatesResults");
-            candidatesResults.empty();
-            for (var i = 1; i <= candidatesCount; i++) {
-                electionInstance.candidates(i).then(function(candidate) {
-                    var id = candidate[0];
-                    var name = candidate[1];
-                    var voteCount = candidate[2];
-                    // Render results
-                    var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
-                    candidatesResults.append(candidateTemplate);
-                });
-            }
+            resultBlock.show();
+            getVotesForCandidatesInCategories(electionInstance, candidatesCnt, 0);
         } else {
             /**
              * Just to be safe, we will remove all result rows
@@ -153,11 +109,13 @@ App = {
              */
             console.log("Contract open");
             $("#voteResultsTable tr").remove();
+            loader.hide();
+            resultBlock.hide();
+            content.show();
         }
         return electionInstance.voters(App.account);
     }).then(function(hasVoted) {
         console.log("has voted:" + hasVoted);
-        console.log(hasVoted);
         // Do not allow a user to vote twice
         if(hasVoted) {
             console.log("user with accont "+ App.account +" already voted");
@@ -165,16 +123,14 @@ App = {
             $('.alert-success').show().html("Thanks for your vote!");
         } else {
             var candidatesSelect = $('#candidatesSelect');
-            candidatesSelect.empty();
-            for (var i = 1; i <= candidatesCount; i++) {
-                electionInstance.candidates(i).then(function(candidate) {
-                    var id = candidate[0];
-                    var name = candidate[1];
-                    var voteCount = candidate[2];
-                    // Render candidate ballot option
-                    var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
-                    candidatesSelect.append(candidateOption);
-                });
+            candidatesSelect.empty
+            numCandidates = returnCandidates();
+            for (var i = 0; i < numCandidates.length; i++) {
+                var id = numCandidates[i].id;
+                var name = numCandidates[i].name;
+                // Render candidate ballot option
+                var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+                candidatesSelect.append(candidateOption);
             }
         }
         $("#loader").hide();
@@ -182,43 +138,89 @@ App = {
     }).catch(function(error) {
         console.warn(error);
     });
-    //
-    // App.contracts.Election.deployed().then(function(instance) {
-    //   electionInstance = instance;
-    //   return electionInstance.candidatesCount();
-    // }).then(function(candidatesCount) {
-    //
-    //   var candidatesSelect = $('#candidatesSelect');
-    //   candidatesSelect.empty();
-    //
-    //   for (var i = 1; i <= candidatesCount; i++) {
-    //     electionInstance.candidates(i).then(function(candidate) {
-    //       var id = candidate[0];
-    //       var name = candidate[1];
-    //       // Render candidate ballot option
-    //       var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
-    //       candidatesSelect.append(candidateOption);
-    //     });
-    //   }
-    //   return electionInstance.voters(App.account);
-    // }).then(function(hasVoted) {
-    //     console.log("has voted:" + hasVoted);
-    //     console.log(hasVoted);
-    //   // Do not allow a user to vote twice
-    //   if(hasVoted) {
-    //       console.log("user with accont "+ App.account +" already voted");
-    //     $('form').hide();
-    //   }
-    //   $("#loader").hide();
-    //   $("#content").show();
-    // }).catch(function(error) {
-    //   console.warn(error);
-    // });
-
 }
 
 };
+var resultsArray = new Array();
+var categories = new Array();
+categories.push("1. Overall Best Teacher");
+categories.push("2. Best Altcoin Picks");
+categories.push("3. Best Guard on Bitcoin Watch");
+categories.push("4. Best ICO Contributions");
+categories.push("5. Best FA Analysis");
+categories.push("6. Best Shot Caller on Margin Plays");
+categories.push("7. Best Contributions to Tools and Education");
+categories.push("8. General Positive Influence on the Pound");
 
+function getVotesForCandidatesInCategories(electionInstance, candidatesCnt, cat) {
+    console.log("candidates count: "+candidatesCnt.toNumber());
+    let candiateTemplate = new Array();
+    for (var i = 1; i <= returnCandidates(); i++) {
+        electionInstance.getCandidateResultsAndInfoFromCategory(cat,i).then(function(candidate) {
+            var id = candidate[0];
+            var name = candidate[1];
+            var voteCount = candidate[2];
+            if(voteCount > 0) {
+            // Render results
+                $('#cat'+cat+'Result').show();
+                candiateTemplate += "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>";
+                console.log("name: "+name);
+                // App.renderResults(c,candidateTemplate)
+                console.log("candidate template string: "+candiateTemplate);
+            }
+            if(id == 0 && cat < 8) {
+                //i-1 should be last candidate with a vote now
+                if(candiateTemplate.length > 0) {
+                    resultsArray[cat] = candiateTemplate;
+                    console.log("result array for cat: "+cat+" : "+resultsArray[cat]);
+                    $('#candidatesResults'+cat).append(candiateTemplate);
+                    $('#resTable').show();
+                    cat++;
+                    getVotesForCandidatesInCategories(electionInstance, candidatesCnt, cat);
+                    addNewCategoryResultTable(cat);
+                }
+            }
+        });
+    }
+}
+function addNewCategoryResultTable(cat) {
+    tblStr = `
+            <div class="categoryResult" id="cat`+cat+`Result" style="display: none">
+                <h3 class='text-center category' categoryId=`+cat+`>`+categories[cat]+`</h1>
+                 <table class='table' id='resTable'>
+                  <thead>
+                    <tr>
+                      <th scope='col'>#</th>
+                      <th scope='col'>Name</th>
+                      <th scope='col'>Votes</th>
+                    </tr>
+                  </thead>
+                  <tbody id='candidatesResults`+cat+`'>
+                  </tbody>
+                </table>
+            </div>`;
+    $('#resultsBlock').append(tblStr);
+}
+//@TODO: parse Candidates from membership group and insert here
+function returnCandidates() {
+    var candidates = new Array();
+    candidate1 =  {
+        id: 1,
+        name : "Dog 1"
+    };
+    candidate2 =  {
+        id: 2,
+        name : "Dog 2"
+    };
+    candidate3 =  {
+        id: 3,
+        name : "Dog 3"
+    };
+    candidates.push(candidate1);
+    candidates.push(candidate2);
+    candidates.push(candidate3);
+    return candidates;
+}
 $(function() {
   $(window).load(function() {
     App.init();
